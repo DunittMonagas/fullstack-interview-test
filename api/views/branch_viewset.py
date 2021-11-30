3ea277030fc13c1c314a0ca0732119b50d3d55b9
Dunitt Monagas <dunitt@example.com>
2021-11-30 03:02:12 -0400

    refactor(BranchViewSet): connects the api (endpoint "/branches/") to the GitPython controller

--- api/views/branch_viewset.py
+++ api/views/branch_viewset.py
@@ -5,21 +5,28 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 
+from utils.git_controller import GitController
+
+
 class BranchViewSet(viewsets.ViewSet):
     """
+    Interface to access the branches.
 
+    Allowed methods: GET
+
+    * list: Name of all branches.
+    * retrieve: A branch in detail.
     """
     def list(self, request):
-        dummy_data = [
-            {'name': 'Branch A', 'description': 'Description A'}, 
-            {'name': 'Branch B', 'description': 'Description B'}, 
-            {'name': 'Branch C', 'description': 'Description C'}, 
-            {'name': 'Branch D', 'description': 'Description D'}, 
-            {'name': 'Branch E', 'description': 'Description E'}, 
-        ]
+        """
+        Returns the names of all local branches in the repository.
+        """
+        git_controller = GitController()
+        branches = git_controller.get_branches()
+        branches = [current_branch.name for current_branch in branches]
 
         return Response({
-            'branches': dummy_data, 
+            'branches': branches, 
         })
 
     def retrieve(self, request, pk=None):
