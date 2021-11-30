2221a4bb10277047595d6b423451c45da82163cb
Dunitt Monagas <dunitt@example.com>
2021-11-30 04:16:08 -0400

    refactor(BranchViewSet): connects the api (endpoint "/branches/pk=branch_name") to the appropriate GitPython controller

--- api/views/branch_viewset.py
+++ api/views/branch_viewset.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 
-from utils.git_controller import GitController
+from utils.git_controller import GitController, BranchNotFountError
 
 
 class BranchViewSet(viewsets.ViewSet):
@@ -30,21 +30,39 @@ class BranchViewSet(viewsets.ViewSet):
         })
 
     def retrieve(self, request, pk=None):
-        dummy_data = [
-            {'name': 'Branch A', 'description': 'Description A'}, 
-            {'name': 'Branch B', 'description': 'Description B'}, 
-            {'name': 'Branch C', 'description': 'Description C'}, 
-            {'name': 'Branch D', 'description': 'Description D'}, 
-            {'name': 'Branch E', 'description': 'Description E'}, 
-        ]
+        """
+        Returns detailed information of the requested branch.
 
-        for branch in dummy_data:
-            if pk == branch['name']:
-                return Response(branch)
-        
-        return Response(
-            {'error': f'Branch {pk} not found.'},
-            status=status.HTTP_404_NOT_FOUND, 
-        )
+        All commits of the branch with:
+            * hexsha (hex version of sha)
+            * Author
+            * Messages per commit (first line of the message)
+            * timestamps (int seconds since epoch is the committed DateTime)
+        """
 
+        git_controller = GitController()
+
+        try:
+            branch = git_controller.get_branch(pk)
+
+        except BranchNotFountError:
+            return Response(
+                {'error': f'Branch {pk} not found.'},
+                status=status.HTTP_404_NOT_FOUND, 
+            )
+
+        commits = []
+        for current_commit in branch['commits']:
+            commits.append({
+                'hexsha': current_commit.hexsha, 
+                'summary': current_commit.summary, 
+                'name': current_commit.author.name,
+                'timestamps': current_commit.committed_date, 
+            })
+
+        return Response({
+            'branch': branch['branch'], 
+            'head': branch['head'].hexsha, 
+            'commits': commits, 
+        })
 
