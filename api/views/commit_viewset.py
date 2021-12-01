9beb3b519fc3c510ffedc86788774df22dd30221
Dunitt Monagas <dunitt@example.com>
2021-12-01 06:24:55 -0400

    docs(CommitViewSet): document the endpoint "/commit/pk=hexsha"

--- api/views/commit_viewset.py
+++ api/views/commit_viewset.py
@@ -11,10 +11,24 @@ from utils.git_controller import GitController
 
 class CommitViewSet(viewsets.ViewSet):
     """
+    Interface to access the commits.
 
+    Allowed methods: GET
+
+    * retrieve: A commit in detail.
     """
     def retrieve(self, request, pk=None):
+        """
+        Returns detailed information of the requested commit.
 
+        A commit has the following fields:
+            * hexsha (hex version of sha)
+            * message
+            * name (atuhor)
+            * email
+            * timestamps (int seconds since epoch is the committed DateTime)
+            * number of files changed
+        """
         git_controller = GitController()
         
         try:
