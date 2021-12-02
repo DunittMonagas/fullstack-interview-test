815c798a16266aba7f60cb1bdfc254d4d4cffaf1
Dunitt Monagas <dunitt@example.com>
2021-12-02 12:20:59 -0400

    refactor(pull request serializer): adds some validations to the pull request view
    * Verify that the received "base_branch" and "compare_branch" branches really exist.
    * Verify that a pull request cannot be created with "closed" status.

--- api/serializers/pull_request_serializer.py
+++ api/serializers/pull_request_serializer.py
@@ -1,21 +1,35 @@
 
 
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
 
 
 from ..models import PullRequest
+from utils.git_controller import GitController
 
 
 class PullRequestSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = (
-            'id', 
-            'author', 
-            'title', 
-            'description', 
-            'status', 
-            'base_branch', 
-            'compare_branch',
-        )
+        fields = "__all__"
         model = PullRequest
+
+    def validate_status(self, value):
+        """
+        """
+        if value == PullRequest.Status.CLOSED:
+            raise ValidationError('Cannot create a pull request with status closed.')
+
+        return value
+
+    def validate(self, data):
+        git_controller = GitController()
+        all_branches = [branch.name for branch in git_controller.get_branches()]
+
+        if data['base_branch'] not in all_branches:
+            raise ValidationError(f"{data['base_branch']} does not exist.")
+
+        if data['compare_branch'] not in all_branches:
+            raise ValidationError(f"{data['compare_branch']} does not exist.")
+
+        return data
 
