ce90df5a3dcbc8fa93f81f178aa6bbdd6af75871
Dunitt Monagas <dunitt@example.com>
2021-12-05 20:48:48 -0400

    feat: add Cross-Origin Resource Sharing (CORS)

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd-party apps
     'rest_framework',
+    'corsheaders', 
     # Local
     'accounts', 
     'api',
@@ -57,12 +58,20 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
+
 
 ROOT_URLCONF = 'config.urls'
 
