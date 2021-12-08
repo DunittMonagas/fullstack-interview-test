92f0e52856d8d75f6f33decaf78da7b56bb5784f
Dunitt Monagas <dunitt@example.com>
2021-12-08 10:13:34 -0400

    feat(pull request): close a pull request listed

--- frontend/src/components/PullRequest/index.js
+++ frontend/src/components/PullRequest/index.js
@@ -30,6 +30,7 @@ export default class PullRequest extends Component {
         const response = await axios.get('/api/v1.0/pull-request/')
         // console.log(response.data);
         const pullRequestData = response.data.map((pullRequest, index) => ({
+            "id": pullRequest["id"],
             "baseBranch": pullRequest["base_branch"],
             "compareBranch": pullRequest["compare_branch"],
             "author": pullRequest["author"],
@@ -68,13 +69,61 @@ export default class PullRequest extends Component {
         this.getOptions()
     }
 
+    handleClose = (id) => {
+
+        axios.defaults.baseURL = 'http://localhost:8000';
+        axios.defaults.headers.common['Accept'] = 'application/json';
+        axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+        const objIndex = this.state.pullRequestData.findIndex((
+            element => element.id == id
+        ));
+
+        const oldPullRequest = this.state.pullRequestData[objIndex];
+
+        const pullRequest = {
+            "id": oldPullRequest["id"],
+            "base_branch": oldPullRequest["baseBranch"],
+            "compare_branch": oldPullRequest["compareBranch"],
+            "author": oldPullRequest["author"],
+            "title": oldPullRequest["title"],
+            "description": oldPullRequest["description"],
+            "conflict": oldPullRequest["conflict"],
+            "conflict_description": oldPullRequest["conflictDescription"],
+            status: 'CL',
+        };
+
+        axios.put(`/api/v1.0/pull-request/${id}/`, pullRequest)
+            .then(response => {
+
+                let items = [...this.state.pullRequestData];
+
+                let item = {
+                    ...items[objIndex],
+                    pullRequestStatus: 'CL'
+                }
+
+                items[objIndex] = item;
+
+                this.setState({items});
+            })
+            .catch(error => {
+                alert("Error!");
+            });
+    };
+
+    handleMerge = (id) => {
+
+    };
+
     render() {
 
         return (    
             <>
-                {this.state.pullRequestData.map((pullRequest, index) => {
+                {this.state.pullRequestData.map((pullRequest) => {
                     
                     const {
+                        id,
                         baseBranch,
                         compareBranch,
                         author,
@@ -84,11 +133,9 @@ export default class PullRequest extends Component {
                         conflict,
                         conflictDescription,
                     } = pullRequest;
-                    // const {summary, name, hexsha, timestamps} = pullRequest
-                    // const date = new Date(timestamps * 1000).toLocaleString()
 
                     return (
-                        <ul key={index} className="list-group list-group-horizontal">
+                        <ul key={id} className="list-group list-group-horizontal">
                             <li className="list-group-item">{baseBranch}</li>
                             <li className="list-group-item">{compareBranch}</li>
                             <li className="list-group-item">{author}</li>
@@ -97,6 +144,20 @@ export default class PullRequest extends Component {
                             <li className="list-group-item">{pullRequestStatus}</li>
                             {conflict &&
                                 <li className="list-group-item">{conflictDescription}</li>
+                            }
+                            {pullRequestStatus === 'OP' &&
+                                <button type="button" className="btn btn-success btn-sm" onClick={() => this.handleMerge(id)}>Merge</button>
+                                // <button type="button" className="btn btn-danger">Close</button>
+                            }
+                            {pullRequestStatus === 'OP' &&
+                                // <button type="button" className="btn btn-success">Merge</button>
+                                <button type="button" className="btn btn-danger btn-sm" onClick={() => this.handleClose(id)}>Close</button>
+                            }
+                            {pullRequestStatus === 'MD' &&
+                                <button type="button" className="btn btn-success btn-sm" disabled>Merged</button>
+                            }
+                            {pullRequestStatus === 'CL' &&
+                                <button type="button" className="btn btn-danger btn-sm" disabled>Closed</button>
                             }
                         </ul>
                     );
